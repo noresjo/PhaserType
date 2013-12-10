680051d83461f43a7650bb9ad57578d8acf98590
noresjo <noresjo@gmail.com>
2013-12-10 23:30:53 +0100

    latestNotSoGreatest

--- app.js
+++ app.js
@@ -9,27 +9,52 @@ var PhaserModule = Phaser;
 var game;
 
 window.onload = function () {
-    var state = new Battle();
+    var state = new BattleState();
 
     game = new PhaserModule.Game(640, 480, Phaser.AUTO, 'phaser-example', state, false, false);
 };
 
-var Battle = (function (_super) {
-    __extends(Battle, _super);
-    function Battle() {
+var Unit = (function (_super) {
+    __extends(Unit, _super);
+    function Unit() {
         _super.apply(this, arguments);
     }
-    Battle.prototype.preload = function () {
+    return Unit;
+})(Phaser.Sprite);
+
+var BattleState = (function (_super) {
+    __extends(BattleState, _super);
+    function BattleState() {
+        _super.apply(this, arguments);
+    }
+    BattleState.prototype.preload = function () {
         game.load.image('mushroom', 'assets/mushroom2.png');
     };
 
-    Battle.prototype.create = function () {
+    BattleState.prototype.create = function () {
         //  This simply creates a sprite using the mushroom image we loaded above and positions it at 200 x 200
         this.mushroom = game.add.sprite(200, 200, 'mushroom');
         this.counter = 0;
+        this.friendlies = new Array();
+
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+
+        this.friendlies.forEach(function (u) {
+            return game.add.existing(u);
+        });
+
+        var currentUnit = this.friendlies[0];
+        var tweenProperties = { alpha: 1 };
+        currentUnit.alpha = 0;
+        var currentFriendlyTween = game.add.tween(currentUnit);
+
+        currentFriendlyTween.to(tweenProperties, 1000, (new Phaser.Easing()).Linear.None, true, 0, true);
     };
 
-    Battle.prototype.update = function () {
+    BattleState.prototype.update = function () {
         var kSpace = game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR);
         var kLeft = game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
         var kRight = game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
@@ -49,6 +74,6 @@ var Battle = (function (_super) {
             this.mushroom.alpha = Math.sin(this.counter) * 0.5 + 0.5;
         }
     };
-    return Battle;
+    return BattleState;
 })(PhaserModule.State);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -4,14 +4,19 @@ import PhaserModule = Phaser;
 var game: Phaser.Game;
 
 window.onload = () => {
-    var state = new Battle();
+    var state = new BattleState();
 
     game = new PhaserModule.Game(640, 480, Phaser.AUTO, 'phaser-example', state, false, false);
 }
 
-class Battle extends PhaserModule.State {
+class Unit extends Phaser.Sprite {
+}
+
+class BattleState extends PhaserModule.State {
     mushroom: Phaser.Sprite;
     counter: number;
+
+    friendlies: Unit[];
 
     preload() {
         game.load.image('mushroom', 'assets/mushroom2.png');
@@ -21,6 +26,22 @@ class Battle extends PhaserModule.State {
         //  This simply creates a sprite using the mushroom image we loaded above and positions it at 200 x 200
         this.mushroom = game.add.sprite(200, 200, 'mushroom');
         this.counter = 0;
+        this.friendlies = new Array<Unit>();
+
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+        this.friendlies.push(new Unit(game, 10, 100 * (this.friendlies.length + 1), 'mushroom', 0));
+
+        this.friendlies.forEach((u) => game.add.existing(u));
+
+        var currentUnit = this.friendlies[0];
+        var tweenProperties = { alpha: 1 };
+        currentUnit.alpha = 0;
+        var currentFriendlyTween = game.add.tween(currentUnit);
+
+        currentFriendlyTween.to(tweenProperties, 1000, (new Phaser.Easing()).Linear.None, true, 0, true);
+
     }
 
     update() {
